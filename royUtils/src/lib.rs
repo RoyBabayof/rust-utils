eaae719dd1b7f2d74b3b5e34f4cb8ac710080db1
unknown <roybabayof@gmail.com>
2026-08-07 22:34:03 +0300

    added the utils lib

---
+++ royUtils/src/lib.rs
@@ -0,0 +1,32 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
+// To Use library declare file path under your dependencies section in your Cargo.toml file.
+// By convention use the royUtils in the name declaration.
+
+pub fn write_to_file(file_path: &String, data: &String) -> std::io::Result<()>{
+    let mut file = OpenOptions::new().append(true).create(true).open(file_path)?;
+    writeln!(file, "Hello, world!")?;
+    Ok(()) // function returns an err or ok signal
+}
+
+pub fn delete_from_file(file_path: &String) -> std::io::Result<()>{
+    std::fs::remove_file(file_path)?;
+    Ok(());
+}
+
+
+#[cfg(test)]
+mod test{
+    #[test]
+    fn test_write_to_file(){
+        let r = write_to_file(&String::from("path.txt"), &String::from("Hello, world!"));
+        assert!(r.is_ok());
+    }
+
+    #[test]
+    fn test_delete_from_file(){
+        let r = delete_from_file(&String::from("path.txt"));
+        assert!(r.is_ok());
+    }
+}
